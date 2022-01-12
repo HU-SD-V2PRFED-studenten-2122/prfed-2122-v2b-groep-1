e1305ec780d7ca102841179ed8bb3c3ea5906286
koen1508 <74555950+koen1508@users.noreply.github.com>
2022-01-12 10:30:13 +0100

    back-end
    back-end application added

---
+++ src/main/java/hu/BezemConversieTool/BezemConversieToolApplication.java
@@ -0,0 +1,12 @@
+package hu.BezemConversieTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezemConversieToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BezemConversieToolApplication.class, args);
+	}
+}
---
+++ src/test/java/hu/BezemConversieTool/BezemConversieToolApplicationTests.java
@@ -0,0 +1,13 @@
+package java.hu.BezemConversieTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BezemConversieToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
